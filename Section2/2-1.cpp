2a398f9a952bb3659157636f9af35b4cbeba8d79
Mio-K0408 <mio.love.k@gmail.com>
2023-03-13 22:44:53 +0900

    2023.3.13

---
+++ Section2/2-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+// 1重ループ
+int main(){
+    int N;
+    cin >> N;
+    
+    int count = 0;
+    for(int i = 0; i < N; ++i) {
+        ++count;
+    }
+}
---
+++ Section2/2-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//2重ループ
+int main(){
+    int N;
+    cin >> N;
+    
+    int count = 0;
+    for(int i = 0; i < N; ++i) {
+        for(int j = 0; j < N; j++){
+            ++count;
+
+        }
+    }
+}

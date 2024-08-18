69a228994be66b651adc7b519b9d00036b006991
Mio-K0408 <mio.love.k@gmail.com>
2024-08-18 16:03:06 +0900

    アルゴ式で基本構文練習8/18

--- beginners/algo/tansaku.cpp
+++ beginners/algo/tansaku.cpp
@@ -8,6 +8,10 @@ using namespace std;
 	out:No
 */
 int main() {
+  // -----------------------------------------
+  /*
+    探索1
+  */
   // データを受け取る
   string S;
   cin >> S;
@@ -27,4 +31,25 @@ int main() {
   if (flag) cout << "Yes" << endl;
   else cout << "No" << endl;
   return 0;
+
+  // -----------------------------------------
+  /*
+    探索2
+  */
+  // データを受け取る
+  int N;
+  cin >> N; 
+  vector<int> A(N); 
+  for (int i=0; i<N; ++i) cin >> A[i];
+
+  // 線形探索
+  vector<int> count(9);  
+  for (int i=0; i<N; ++i) {   
+    count[A[i]-1]++;
+  }
+  
+  // 答えを出力する
+  for (int i=0; i<9; ++i) {
+    cout << count[i] << endl;
+  }
 }
